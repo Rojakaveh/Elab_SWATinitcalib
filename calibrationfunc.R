16f529c3a1857d38c7b92865104c8aca6fdcc63a
Roja Kaveh Garna <51385233+Rojakaveh@users.noreply.github.com>
2022-05-23 15:31:09 -0400

    Create calibrationfunc.R

---
+++ calibrationfunc.R
@@ -0,0 +1,29 @@
+swat_objective_function_rch_new=function (x, calib_range, calib_params, flowgage, rch,save_results=F)
+{
+  pacman::p_load(SWATmodel,dplyr,EcoHydRology,topmodel,utils)
+  calib_params$current <- x
+  tmpdir=as.character(as.integer((runif(1)+1)*10000))
+  tmpdir=paste(c(format(Sys.time(), "%s"),tmpdir,Sys.getpid()),sep="",collapse="")
+  print(tmpdir)
+  dir.create(tmpdir)
+  file.copy(list.files(),tmpdir)
+  setwd(tmpdir)
+  file.remove(list.files(pattern="output."))
+  alter_filesfn(calib_params)
+  libarch = if (nzchar(base::version$arch)) paste("libs", base::version$arch, sep = "/") else "libs"
+  swatbin <- "rswat2012.exe"
+  junkout=system(shQuote(paste(path.package("SWATmodel"), libarch, swatbin, sep = "/")),intern = TRUE)
+  start_year = read.fortran(textConnection(readLines("file.cio")[9]), "f20")
+  load("readSWAT.R")
+  outdata = readSWAT("rch",".")
+  outdata$FLOW_OUTmm=(outdata$FLOW_OUTcms*24*3600)/(outdata$AREAkm2*1000)
+  test2 = subset(outdata, outdata$RCH == rch)
+  test3 = merge(flowgage$flowdata, test2, all = F)
+  NS = topmodel::NSeff(test3$Qmm, test3$FLOW_OUTmm)
+  print(NS)
+  if(save_results){file.copy(list.files(),"../")}
+  file.remove(list.files())
+  setwd("../")
+  file.remove(tmpdir)
+  return(abs(NS - 1))
+}
